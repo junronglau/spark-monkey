0a53c4d6f32714536430c9b5f1958297e69c3537
Junrong Lau <Junrong Lau>
2023-03-26 20:29:36 +0800

    Updated setup.py to include numpy version

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
     author='junrong.lau',
     author_email='junronglau@gmai.com',
     description='Python library to analyse and troubleshoot spark jobs performance',
-    install_requires=['pandas', 'tqdm', 'scipy', 'numpy']
+    install_requires=['pandas', 'tqdm', 'scipy', 'numpy>=1.24.2']
 )
