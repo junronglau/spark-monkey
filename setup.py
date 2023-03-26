4d81223cbcbbfd93898ffbae624982975120caf8
Junrong Lau <Junrong Lau>
2023-03-26 20:53:02 +0800

    added config to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='SparkMonkey',
     version='1.01',
-    packages=find_packages() + ['./config'],
+    packages=find_packages(),
     url='',
     license='',
     author='junrong.lau',
