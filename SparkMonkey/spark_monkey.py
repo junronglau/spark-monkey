dcc3ed5f7d7dd162181e18150763e62c59ade684
Junrong Lau <Junrong Lau>
2023-03-27 00:45:18 +0800

    shifted read data path utils to utils.py

--- SparkMonkey/spark_monkey.py
+++ SparkMonkey/spark_monkey.py
@@ -1,13 +1,9 @@
 import configparser
 import itertools
 import json
-import os
-import pkgutil
 import re
 import re as regex
-import sys
 from ast import literal_eval
-from pkgutil import get_loader
 
 import pandas as pd
 import requests as re
@@ -17,39 +13,12 @@ from SparkMonkey.scorer.scorer import Scorer
 from SparkMonkey.utils.utils import Utils
 
 
-def get_data_smart(package, resource, as_string=False):
-    """Rewrite of pkgutil.get_data() that actually lets the user determine if data should
-    be returned read into memory (aka as_string=True) or just return the file path.
-    """
-
-    loader = get_loader(package)
-    if loader is None or not hasattr(loader, 'get_data'):
-        return None
-    mod = sys.modules.get(package) or loader.load_module(package)
-    if mod is None or not hasattr(mod, '__file__'):
-        return None
-
-    # Modify the resource name to be compatible with the loader.get_data
-    # signature - an os.path format "filename" starting with the dirname of
-    # the package's __file__
-    parts = resource.split('/')
-    parts.insert(0, os.path.dirname(mod.__file__))
-    resource_name = os.path.join(*parts)
-    if as_string:
-        return loader.get_data(resource_name)
-    else:
-        return resource_name
-
-
 class SparkMonkey:
     def __init__(self, databricks_host_url):
         self.utils = Utils()
         self.scorer = Scorer()
         self.config = configparser.ConfigParser()
-        print(get_data_smart(__name__, "config/config.cfg"))
-        read_status = self.config.read(get_data_smart(__name__, "config/config.cfg"))
-        print(read_status)
-
+        self.config.read(self.utils.get_data_smart(__name__, "config/config.cfg"))
 
         """
         AUTHENTICATION
--- SparkMonkey/utils/utils.py
+++ SparkMonkey/utils/utils.py
@@ -1,4 +1,7 @@
+import os
 import re as regex
+import sys
+from pkgutil import get_loader
 
 import IPython as ip
 from pyspark.dbutils import DBUtils
@@ -44,6 +47,29 @@ class Utils:
 
     def get_dbutils(self):
         return self._get_dbutils(self.spark)
+
+    def get_data_smart(package, resource, as_string=False):
+        """Rewrite of pkgutil.get_data() that actually lets the user determine if data should
+        be returned read into memory (aka as_string=True) or just return the file path.
+        """
+
+        loader = get_loader(package)
+        if loader is None or not hasattr(loader, 'get_data'):
+            return None
+        mod = sys.modules.get(package) or loader.load_module(package)
+        if mod is None or not hasattr(mod, '__file__'):
+            return None
+
+        # Modify the resource name to be compatible with the loader.get_data
+        # signature - an os.path format "filename" starting with the dirname of
+        # the package's __file__
+        parts = resource.split('/')
+        parts.insert(0, os.path.dirname(mod.__file__))
+        resource_name = os.path.join(*parts)
+        if as_string:
+            return loader.get_data(resource_name)
+        else:
+            return resource_name
 
     def get_displayHTML(self):
         return self._get_displayHTML()
