97b7882ceeb6e02889efd8664eabb33e6f932ca7
Junrong Lau <Junrong Lau>
2023-03-26 18:09:10 +0800

    Fixed double specification of packages in setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='SparkMonkey',
     version='1.01',
-    packages=['spark_monkey'],
+    packages=find_packages(include=['spark_monkey', 'spark_monkey.*']),
     url='',
     license='',
     author='junrong.lau',
     author_email='junronglau@gmai.com',
     description='Python library to analyse and troubleshoot spark jobs performance',
-    packages=find_packages(include=['spark_monkey', 'spark_monkey.*']),
     install_requires=['pandas', 'tqdm', 'scipy', 'numpy']
 )
